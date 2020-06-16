03413f99b3d215654a318a4f71d0315b9f0e6976
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-16 15:13:08 +0100

    add TeamSelect

---
+++ src/components/TeamSelect.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TeamSelect = (props) =>{
+
+
+return(
+<select id="team-select" >
+<option>Choose a team...</option>
+
+</select>
+
+)
+}
+export default TeamSelect
