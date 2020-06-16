2ba55b9305f74bea065abcce81f1f0c0bf944f76
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-16 15:10:16 +0100

    fetch data from Api

--- src/containers/TeamContainer.js
+++ src/containers/TeamContainer.js
@@ -1,8 +1,28 @@
 import React, {Component, Fragment} from "react";
 
 class TeamContainer extends Component{
+constructor(props){
+  super(props)
+    this.state = {
+      teams:[],
+      selectedTeam: ""
+
+    }
+  }
+
+  componentDidMount(){
+    const token = "9b41bcd09c2e4c94baad09aedc40bfc4";
+    const url = 'http://api.football-data.org/v2/teams';
+    fetch(url, { headers: {'X-Auth-Token': token} })
+  .then(response => response.json())
+  .then(data => this.setState({teams: data.teams}))
+  .catch(err => console.error(err));
+  }
+
+
+
 render(){
-  return()
+  return( null)
 }
 }
 export default TeamContainer
